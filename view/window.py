be28db9f81fa66e9410fd480475a2c099e0a15e4
Alec Petit-Siejak <alecpetitsiejak@gmail.com>
2024-12-05 10:00:26 +0100

    Update

--- view/window.py
+++ view/window.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+from model.playlist import Playlist
+
 #import main
 #from view import PlaylistView
 
@@ -15,18 +17,45 @@ class Window(tk.Tk):
         # Initialize playlist view
         #self.playlist_menu = PlaylistView(self)
 
-        # Create the playlist
+
+        ## CONTROL FRAME
+        controls_frame = tk.Frame(self)
+        controls_frame.pack()
+
+
+        ## BUTTON
+        # Create the add button
+        self.btn_add = tk.Button(controls_frame, text="Add")
+        self.btn_add.grid(row=0, column=0, padx=10)
+
+
+        # Initialiaze playlist data
+        self.pl = Playlist()
+        self.pl.add("https://youtu.be/rrrbO-MaerM?si=VHbSvdLtzcV602Dc")
+        self.pl.add("https://youtu.be/rrrbO-MaerM?si=VHbSvdLtzcV602Dc")
+        self.pl.add("https://youtu.be/rrrbO-MaerM?si=VHbSvdLtzcV602Dc")
+        self.pl.add("https://youtu.be/rrrbO-MaerM?si=VHbSvdLtzcV602Dc")
+        self.pl.add("https://youtu.be/rrrbO-MaerM?si=VHbSvdLtzcV602Dc")
+
+        # Create the listbox
         self.listbox = tk.Listbox(self)
-        #self.fill_playlist()
         self.listbox.pack(fill="x", padx=10, pady=10)
+
+        # Fill the playlist
+        self.fill_playlist()
 
         # Run
         self.mainloop()
 
+
+
     # Function that recover videos from the list and displays them in the playlist
-    #def fill_playlist(self):
-        #pl = main.playlist
-        #self.listbox.insert(1, pl.access_by_position(1))
+    def fill_playlist(self):
+        video_pos = 1
+
+        while self.pl.access_by_position(video_pos) is not None:
+            self.listbox.insert(tk.END, self.pl.access_by_position(video_pos))
+            video_pos += 1
 
 
 
